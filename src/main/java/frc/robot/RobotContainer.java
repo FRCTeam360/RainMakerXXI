54c9b99057c0ef03ea23e0740cd1aa321e05fea5
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2020-01-14 18:32:35 -0800

    Changed kF to 1023.0/100000 to 1023.0/60250 because 60250 was found to be the best value for the shooter
    Co-Authored-By: danieljpuratich <danieljpuratich@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,8 +40,6 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
-  //try this @Daniel
-
   /**
    * Use this method to define your button->command mappings.  Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
