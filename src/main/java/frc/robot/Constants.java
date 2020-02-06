ab9aa3b76e45d5e9aa4ef93704aec60eaa2ce210
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-06 09:19:52 -0800

    Added exampleTrajectory to TrajectoryConstants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -107,7 +107,27 @@ public final class Constants {
         	// Add kinematics to ensure max speed is actually obeyed
         	.setKinematics(AutoConstants.kDriveKinematics)
         	// Apply the voltage constraint
-        	.addConstraint(autoVoltageConstraint);
+            .addConstraint(autoVoltageConstraint);
+            
+		// An example trajectory to follow.  All units in meters.
+        private static final Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            List.of(
+                new Translation2d(1, 1),
+                new Translation2d(2, -1)
+            ),
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            // Pass config
+            config
+        );
+        //Left Auto Trajectory
+
+        //Middle Auto Trajectory
+        
+        //Right Auto Trajectory
         
     }
 
