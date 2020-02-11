0ae2c1d621bc4fae6d8074973831b8c1e125349a
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-11 13:15:02 -0800

    Finished SparkMaxes Subsystem

--- src/main/java/frc/robot/subsystems/SparkMaxes.java
+++ src/main/java/frc/robot/subsystems/SparkMaxes.java
@@ -7,14 +7,45 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SparkMaxes extends SubsystemBase {
-  /**
-   * Creates a new SparkMaxes.
-   */
-  public SparkMaxes() {
 
+  private static CANSparkMax spark1;
+  private static CANSparkMax spark2;
+  private static CANSparkMax spark3;
+  private static CANSparkMax spark4;
+  private static CANSparkMax spark5;
+  private static CANSparkMax spark6;
+  
+  public CANEncoder enc1;
+  public CANEncoder enc2;
+  public CANEncoder enc3;
+  public CANEncoder enc4;
+  public CANEncoder enc5;
+  public CANEncoder enc6;
+
+  public SparkMaxes() {
+    spark1 = new CANSparkMax(1 , MotorType.kBrushless);
+    spark2 = new CANSparkMax(2 , MotorType.kBrushless);
+    spark3 = new CANSparkMax(3 , MotorType.kBrushless);
+    spark4 = new CANSparkMax(4 , MotorType.kBrushless);
+    spark5 = new CANSparkMax(5 , MotorType.kBrushless);
+    spark6 = new CANSparkMax(6 , MotorType.kBrushless);
+  }
+
+  public void runPercent (double pPower) {
+    spark1.set(pPower);
+    spark2.set(pPower);
+    spark3.set(pPower);
+    spark4.set(pPower);
+    spark5.set(pPower);
+    spark6.set(pPower);
   }
 
   @Override
