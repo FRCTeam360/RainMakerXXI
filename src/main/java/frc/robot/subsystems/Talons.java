e0a5e02781ab41b933a6aaa5579a29bf1bb5bccf
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-11 13:48:04 -0800

    More to Talon Subsystem

--- src/main/java/frc/robot/subsystems/Talons.java
+++ src/main/java/frc/robot/subsystems/Talons.java
@@ -9,17 +9,31 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.InvertType;
+//import com.ctre.phoenix.motorcontrol.ControlMode;
+//import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+//import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Talons extends SubsystemBase {
-  /**
-   * Creates a new Talons.
-   */
-  public Talons() {
+  
+  private TalonSRX talon7;
+  private TalonSRX talon8;
+  private TalonSRX talon9;
+  private TalonSRX talon10;
+  private TalonSRX talon11;
+  private TalonSRX talon12;
 
+  public Talons() {
+    talon7 = new TalonSRX(7);
+    talon8 = new TalonSRX(8);
+    talon9 = new TalonSRX(9);
+    talon10 = new TalonSRX(10);
+    talon11 = new TalonSRX(11);
+    talon12 = new TalonSRX(12);
+  }
+
+  public void runPercent (double pPower) {
+		
   }
 
   @Override
