8b57b642b17bd59432a636219ff45aadbde150ff
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-11 13:17:48 -0800

    Added Talon Imports to Talons Subsystem

--- src/main/java/frc/robot/subsystems/Talons.java
+++ src/main/java/frc/robot/subsystems/Talons.java
@@ -9,6 +9,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 public class Talons extends SubsystemBase {
   /**
    * Creates a new Talons.
