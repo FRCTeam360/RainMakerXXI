023307c680c2aa176d5d3800075671669995bd5b
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-14 13:09:55 -0800

    Refined RobotContainer Imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
