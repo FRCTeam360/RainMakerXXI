c0a02e7b9f221128ff3ee70b02a4748fdbb75575
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-15 12:10:11 -0800

    Refined RobotContainer order/syntax
    also prepared for testing the next electrical board

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,51 +7,29 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
 
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
 
-  private final Pneumatics pneumatics = new Pneumatics();
-  //private final SparkMaxes sparkMaxes = new SparkMaxes();
-  //private final Talons talons = new Talons();
-  private final Solenoids solenoids = new Solenoids();
+  //private final Pneumatics pneumatics = new Pneumatics();
+  //private final Solenoids solenoids = new Solenoids();
+  private final SparkMaxes sparkMaxes = new SparkMaxes();
+  private final Talons talons = new Talons();
 
-  private final Pressurize pressurize = new Pressurize(pneumatics);
-  //private final RunMotors runMotors = new RunMotors(talons, sparkMaxes);
-  private final FireSolenoids fireSolenoids = new FireSolenoids(solenoids);
+  //private final Pressurize pressurize = new Pressurize(pneumatics);
+  //private final FireSolenoids fireSolenoids = new FireSolenoids(solenoids);
+  private final RunMotors runMotors = new RunMotors(talons, sparkMaxes);
 
   public RobotContainer() {
-
-    pneumatics.setDefaultCommand(pressurize);
-    //talons.setDefaultCommand(runMotors); //no need to do: sparkMaxes.setDefaultCommand(runMotors);
-    solenoids.setDefaultCommand(fireSolenoids);
-
-    configureButtonBindings();
+    //pneumatics.setDefaultCommand(pressurize);
+    //solenoids.setDefaultCommand(fireSolenoids);
+    talons.setDefaultCommand(runMotors); //no need to do: sparkMaxes.setDefaultCommand(runMotors);
   }
 
-  /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-  }
-
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
     return null;
   }
 }
