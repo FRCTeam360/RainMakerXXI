d78e1c054272b76bd8b8d4c5c47febe1d13c9851
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-18 17:26:02 -0800

    preapring to test next robot, commented motors, enabled pneumatics

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,21 +14,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
 
-  //private final Pneumatics pneumatics = new Pneumatics();
-  //private final Solenoids solenoids = new Solenoids();
-  private final SparkMaxes sparkMaxes = new SparkMaxes();
-  private final Talons talons = new Talons();
+  private final Pneumatics pneumatics = new Pneumatics();
+  private final Solenoids solenoids = new Solenoids();
+  //private final SparkMaxes sparkMaxes = new SparkMaxes();
+  //private final Talons talons = new Talons();
   private final Navx navx = new Navx();
 
-  //private final Pressurize pressurize = new Pressurize(pneumatics);
-  //private final FireSolenoids fireSolenoids = new FireSolenoids(solenoids);
-  private final RunMotors runMotors = new RunMotors(talons, sparkMaxes);
+  private final Pressurize pressurize = new Pressurize(pneumatics);
+  private final FireSolenoids fireSolenoids = new FireSolenoids(solenoids);
+  //private final RunMotors runMotors = new RunMotors(talons, sparkMaxes);
   private final NavReadout navReadout = new NavReadout(navx);
 
   public RobotContainer() {
-    //pneumatics.setDefaultCommand(pressurize);
-    //solenoids.setDefaultCommand(fireSolenoids);
-    talons.setDefaultCommand(runMotors); //no need to do: sparkMaxes.setDefaultCommand(runMotors);
+    pneumatics.setDefaultCommand(pressurize);
+    solenoids.setDefaultCommand(fireSolenoids);
+    //talons.setDefaultCommand(runMotors); //no need to do sparkMaxes.setDefaultCommand(runMotors); cuz thats the way it be
     navx.setDefaultCommand(navReadout);
   }
 
