937785cf5edfb48b46210a2ec58a4f6096de299c
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-19 15:00:49 -0800

    Comment Modifications

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,52 +14,29 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import static frc.robot.Constants.inAuto;
 
+//The VR is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the TimedRobot documentation. 
+//If you change the name of this class or the package after creating this project, you must also update the build.gradle file in the project.
 
-
-/**
- * The VR is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
+  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+  public void robotInit() { //This function is run when the robot is first started up and should be used for any initialization code.
+    m_robotContainer = new RobotContainer(); //Instantiate our RobotContainer. This will perform all our button bindings, and put our autonomous chooser on the dashboard.
     CameraServer.getInstance().startAutomaticCapture();
   }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+  
   @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+  public void robotPeriodic() { //This function is called every robot packet, no matter the mode. Use for things wanted in all modes
+    CommandScheduler.getInstance().run();//Runs Command Scheduler, must be called for any command to work
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
+
+
   @Override
-  public void disabledInit() {
+  public void disabledInit() { //This function is called once each time the robot enters Disabled mode.
     m_robotContainer.gDriveTrain().brakeMode(); //Here for the auto testing, want robot to stop after auto sends it full speed into a wall
     //m_robotContainer.gDriveTrain().coastMode(); //When robot is initially disabled, set coast mode
   }
@@ -67,6 +44,8 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {
   }
+
+
 
   @Override
   public void autonomousInit() { //This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
@@ -82,9 +61,12 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.schedule(); //run the auto command
     }
   }
+
   @Override
   public void autonomousPeriodic() { //This function is called periodically during autonomous.
   }
+
+
 
   @Override
   public void teleopInit() {
@@ -96,15 +78,19 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.cancel(); //If you want the autonomous to  continue until interrupted by another command comment it out.
     }
   }
+
   @Override
   public void teleopPeriodic() {    //This function is called periodically during operator control.
   }
+
+
 
   @Override
   public void testInit() {     // Cancels all running commands at the start of test mode.
     m_robotContainer.gDriveTrain().brakeMode(); //Set brake
     CommandScheduler.getInstance().cancelAll(); //Clear all commands
   }
+
   @Override
   public void testPeriodic() { //This function is called periodically during test mode.
   }
