58d4d92aad3aa7d4d15773666c63c41dd44d0a4b
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-19 17:05:48 -0800

    Comment modifications

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -9,21 +9,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-/**
- * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
- * you are doing, do not modify this file except to change the parameter class to the startRobot
- * call.
- */
+//Do NOT add any static variables to this class, or any initialization at all. Unless you know what you are doing, 
+//Do not modify this file except to change the parameter class to the startRobot call.
+ 
 public final class Main {
-  private Main() {
+  private Main() {    //Main initialization function. Do not perform any initialization here.
   }
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
+  public static void main(String... args) {    //If you change your main robot class, change the parameter type.
     RobotBase.startRobot(Robot::new);
   }
 }
