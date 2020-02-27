5f566135e76a1eae41fb7286a842b2d3566ff9fc
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-02-26 16:48:52 -0800

    Modified limelight printouts

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -64,7 +64,8 @@ public class Limelight extends SubsystemBase {
     //SmartDashboard.putNumber("LimelightY", ty.getDouble(0.0));
     //SmartDashboard.putNumber("LimelightArea", ta.getDouble(0.0));
 
-    SmartDashboard.putBoolean("Valid Target", validTarget()); //Comp Setup
-    SmartDashboard.putNumber("LimelightX", tx.getDouble(0.0)); //Comp Setup
+    SmartDashboard.putBoolean("Lime Targ", validTarget()); //Comp Setup
+    SmartDashboard.putNumber("Lime X", tx.getDouble(0.0)); //Comp Setup
+    //Potentially add limelight display here
   }
 }
