38184820806440262e1db305f04b11edd0eb0fec
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-03-10 11:37:29 -0700

    Removed unused methods.

--- src/main/java/frc/robot/commands/RunCamera.java
+++ src/main/java/frc/robot/commands/RunCamera.java
@@ -8,39 +8,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
 import frc.robot.subsystems.Limelight;
 
 public class RunCamera extends CommandBase {
 
   private final Limelight myLimelight;
 
-  /**
-   * Creates a new RunCamera.
-   */
   public RunCamera(Limelight limelight) {
     myLimelight = limelight;
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(myLimelight);
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+  public boolean isFinished() { return false; }
 }
