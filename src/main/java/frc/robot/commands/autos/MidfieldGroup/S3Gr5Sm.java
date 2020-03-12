b310232408631fde66decf7ef3d741fe3e6fdb46
DanielJPuratich <42872560+DanielJPuratich@users.noreply.github.com>
2020-03-12 15:39:13 -0700

    MidfieldGroup autos

---
+++ src/main/java/frc/robot/commands/autos/MidfieldGroup/S3Gr5Sm.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.autos.MidfieldGroup;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+//import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+
+//import frc.robot.Constants.????;
+
+//import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+public class S3Gr5Sm extends SequentialCommandGroup {
+  public S3Gr5Sm( DriveTrain drivetrain, Limelight limelight, Feeder feeder, Shooter shooter, Intake intake ) {
+    super(
+
+    );
+
+  }
+}
