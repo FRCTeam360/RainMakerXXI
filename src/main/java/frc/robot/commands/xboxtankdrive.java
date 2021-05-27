43fa7076ba39a6a9122687f1802f8f9cf1a2ae13
tbmilliren <60276487+tbmilliren@users.noreply.github.com>
2021-05-26 17:33:08 -0700

    Create xboxtankdrive.java
    Started on making changes from joystick.

---
+++ src/main/java/frc/robot/commands/xboxtankdrive.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+import static frc.robot.Constants.OIConstants.*;
+
+public class xboxtankdrive extends CommandBase {
+
+  private final DriveTrain myDriveTrain;
+
+  private final XboxController driverCont;
+
+
+  public xboxtankdrive(DriveTrain driveTrain) {
+
+    driverCont = new XboxController(getYButton);
+    driverCont = new XboxController(getX);
+    driverCont= new XboxController(getY);
+
+    myDriveTrain = driveTrain;
+
+    addRequirements(myDriveTrain); // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  @Override
+  public void initialize() {   // Called when the command is initially scheduled.
+  }
+
+  @Override
+  public void execute() {   // Called every time the scheduler runs while the command is scheduled.
+    if(Math.abs(joyR.getRawAxis(1)) >= .125){
+    	myDriveTrain.driveRMAX(-1 * joyR.getRawAxis(1) * 0.8);
+    }else{
+    	myDriveTrain.driveRMAX(0);
+    }
+    if(Math.abs(joyL.getRawAxis(1)) >= .125){
+      myDriveTrain.driveLMAX(-1 * joyL.getRawAxis(1) * 0.8);
+    }else{
+    	myDriveTrain.driveLMAX(0);
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {   // Called once the command ends or is interrupted.
+  }
+
+  @Override
+  public boolean isFinished() {  // Returns true when the command should end.
+    return false;
+  }
+}
+
